1ad1bf8f72fb3a1021e0e5243c44c7aa448ae10b
MartinsOnuoha <martinsvictor.nd@gmail.com>
2018-10-19 03:40:40 +0100

    create user-profile relationship

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('avatar');
             $table->string('slug');
             $table->boolean('gender');
+            $table->enum('user_type', ['talent', 'investor']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
